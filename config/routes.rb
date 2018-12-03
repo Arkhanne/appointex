382308a4862c9be2c8c3d0362b7f66e41956566a
Arkhanne <eduardbenito@icloud.com>
2018-12-03 16:18:49 +0100

    change root url to signin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resource :session
 
-  get 'root' => 'users#index'
+  get 'root' => 'sessions#new'
   get 'signup' => 'users#new'
   get 'signin' => 'sessions#new'
 end

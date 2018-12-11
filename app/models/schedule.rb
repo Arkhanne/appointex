c575ccfc0e80708bd93aaf7bcb503d9dc53a1258
Edu Benito <eduardbenito@icloud.com>
2018-12-11 12:43:27 +0100

    fixes day 0 bug on schedule configuration

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,6 @@
 class Schedule < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
 
-  validates :week_day, presence: true, inclusion: { in: 1..7 }
+  validates :week_day, presence: true, inclusion: { in: 0..6 }
   validates :hour, presence: true, inclusion: { in: 0..23 }
 end
